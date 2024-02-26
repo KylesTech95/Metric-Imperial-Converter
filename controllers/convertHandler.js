b4be5097aa0cbe74fba1aa3908c04b067492a99b
KylesTech95 <108553476+KylesTech95@users.noreply.github.com>
2024-02-26 15:41:45 -0500

    feat: convertHandler & api.js

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,27 +1,70 @@
 function ConvertHandler() {
   
   this.getNum = function(input) {
-    let result;
-    
-    return result;
+    let result = parseFloat(input.match(/[(\d+)|(\d+)\.\d*]/g).join``);
+    console.log('number: '+result)
+    console.log(result);
+    return result
   };
   
   this.getUnit = function(input) {
-    let result;
-    
+    let result = input.match(/[a-z]/ig).join``;
+    console.log('unit: '+result)
     return result;
   };
   
   this.getReturnUnit = function(initUnit) {
-    let result;
+    let u = initUnit.match(/[a-z]/ig).join``;
+    let d = parseFloat(initUnit.match(/[(\d+)|(\d+)\.\d*]/g).join``);
+
+    let res = this.convert(d,u)
+    res = res.match(/[a-z]/ig).join``;
+
+    console.log(res)
+    return res
     
-    return result;
+  };
+   this.getReturnNum = function(initUnit) {
+    let u = initUnit.match(/[a-z]/ig).join``;
+    let d = parseFloat(initUnit.match(/[(\d+)|(\d+)\.\d*]/g).join``);
+
+    let res = this.convert(d,u)
+    res = parseFloat(res.match(/[(\d+)|(\d+)\.\d*]/g).join``);
+
+    console.log(res)
+    return res
+    
   };
 
   this.spellOutUnit = function(unit) {
-    let result;
-    
+    let u = unit.match(/[a-z]/ig).join``;
+    switch(true){
+      case u=='mi':
+        result = 'miles'
+        break;
+      case u=='lbs':
+        result = 'pounds'
+        break;
+      case u=='gal':
+        result = 'gallons'
+        break;
+        case u=='km':
+        result = 'kilometers'
+        break;
+        case u=='l':
+        result = 'liters'
+        break;
+        case u=='kg':
+        result = 'kilograms'
+        break;
+      default:
+        console.log(undefined);
+        break;
+    }
+    console.log('speell out unit: '+result)
     return result;
+    
+  
   };
   
   this.convert = function(initNum, initUnit) {
@@ -29,13 +72,25 @@ function ConvertHandler() {
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
     let result;
-    
+    switch(true){
+      case initUnit=='mi':
+        result = (miToKm * initNum).toFixed(5)+'km'
+        break;
+      case initUnit=='lbs':
+        result = (lbsToKg * initNum).toFixed(5)+'kg'
+        break;
+      case initUnit=='gal':
+        result = (galToL * initNum).toFixed(5)+'l'
+        break;
+      default:
+        console.log(undefined);
+        break;
+    }
+    console.log('converted: '+result)
     return result;
   };
   
   this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    let result;
-    
     return result;
   };
   
