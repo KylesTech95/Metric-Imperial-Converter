b7f6d2ddbf34d371dbd90daa92d4ff668ab0f5ec
KylesTech95 <108553476+KylesTech95@users.noreply.github.com>
2024-02-26 20:36:44 -0500

    fork: update input

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,40 +1,99 @@
 function ConvertHandler() {
   
   this.getNum = function(input) {
-    let result;
-    
-    return result;
+    return input
   };
   
   this.getUnit = function(input) {
-    let result;
-    
-    return result;
+    return /^l$/i.test(input) ? 'L' : input.toLowerCase()
   };
   
   this.getReturnUnit = function(initUnit) {
-    let result;
-    
-    return result;
+    let res
+    switch(true){
+      case initUnit=='mi':
+        res = 'km'
+        break;
+      case initUnit=='lbs':
+        res = 'kg'
+        break;
+      case initUnit=='gal':
+        res = 'L'
+        break;
+        case initUnit=='km':
+        res = 'mi'
+        break;
+        case initUnit=='L':
+        res = 'gal'
+        break;
+        case initUnit=='kg':
+        res = 'lbs'
+        break;
+      default:
+        console.log(undefined);
+        break;
   };
+  return res
+}
 
   this.spellOutUnit = function(unit) {
-    let result;
-    
-    return result;
+    let res
+    switch(true){
+      case unit=='mi':
+        res = 'miles'
+        break;
+      case unit=='lbs':
+        res = 'pounds'
+        break;
+      case unit=='gal':
+        res = 'gallons'
+        break;
+        case unit=='km':
+        res = 'kilometers'
+        break;
+        case unit=='L':
+        res = 'liters'
+        break;
+        case unit=='kg':
+        res = 'kilograms'
+        break;
+      default:
+        console.log(undefined);
+        break;
+  };
+  return res
   };
   
   this.convert = function(initNum, initUnit) {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
-    let result;
-    
-    return result;
+    let res
+    switch(true){
+      case initUnit == 'mi':
+        res = (miToKm*initNum).toFixed(5)
+        break;
+      case initUnit == 'lbs':
+        res = (lbsToKg*initNum).toFixed(5)
+        break;
+      case initUnit == 'gal':
+        res = (galToL*initNum).toFixed(5)
+        break;
+      case initUnit == 'kg':
+        res = (initNum/lbsToKg).toFixed(5)
+        break;
+      case initUnit == 'L':
+        res = (initNum/galToL).toFixed(5)
+        break;
+      case initUnit == 'km':
+        res = (initNum/miToKm).toFixed(5)
+        break;
+    }
+    return res
   };
   
   this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    let result;
+    let result = {initNum:initNum,initUnit:initUnit,returnNum:returnNum,returnUnit:returnUnit,string:`${initNum} ${this.spellOutUnit(initUnit)} converts to ${returnNum} ${this.spellOutUnit(returnUnit)}`}
     
     return result;
   };
--- routes/api.js
+++ routes/api.js
@@ -3,8 +3,45 @@
 const expect = require('chai').expect;
 const ConvertHandler = require('../controllers/convertHandler.js');
 
-module.exports = function (app) {
-  
-  let convertHandler = new ConvertHandler();
+const valUnit = (u) => {
+  return ['kg','lbs','L','km','gal','mi','l'].includes(u)
+}
 
+module.exports = function (app) {  
+  let convertHandler = new ConvertHandler();
+  const startConvert = (num,unit,res) => {
+    // console.log(num,unit)
+    // res.json({unit:unit,number:num})
+    let n = eval(convertHandler.getNum(num)) // initNum
+    let u = convertHandler.getUnit(unit) //initUnit
+    let return_n = convertHandler.convert(n,u)
+    let return_u = convertHandler.getReturnUnit(u) //return unit
+    let sp_unit = convertHandler.spellOutUnit(return_u)
+    let string = convertHandler.getString(n,u,return_n,return_u)
+    // console.log(typeof n)
+    // console.log(n)
+    // console.log(u)
+    // console.log(return_u)
+    // console.log(sp_unit)
+    // console.log(reuturn_n)
+    console.log(string)
+  
+  }
+
+  app.get('/api/convert',(req,res)=>{
+    const { input } = req.query;
+    let num = (input.match(/[^a-z]/g)||[]).join`` // get numbers in eval form
+    let unit = (input.match(/[a-z]/gi)||[]).join``.toLowerCase(); // get units
+    switch(true){
+      case !num:
+        res.send('invalid number')
+        break;
+      case !unit||!valUnit(unit):
+        res.send('invalid unit')
+        break;
+      default:
+        startConvert(num,unit,res)
+        break;
+    }
+  })
 };
