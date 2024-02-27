786b6750b96351503007fc5b4000e731a2787cf3
KylesTech95 <108553476+KylesTech95@users.noreply.github.com>
2024-02-27 17:31:24 -0500

    set values to Number

--- public/eval.js
+++ public/eval.js
@@ -1,17 +1,13 @@
 function equation(num){
     let n;
-    console.log('equation: '+n)
     if(/\//.test(num)){
     n = num.split`/`
     // console.log(n)
     n=n.reduce((a,b)=>a/b)+''
     if(/\./g.test(n)){
-      console.log('my form of decimal')
-        console.log('decimal')
         n=(+n).toFixed(5)
     }
     else{
-        console.log('no deci')
         n = +n
     }
     // console.log(n)
@@ -28,14 +24,10 @@ return n === Infinity ? false : n
 function MyEval(num){
     let res;
     let arr = num.split``
-    console.log(num)
-    // console.log(arr)
-    // console.log(arr.length)
+
     try{
       if((/[\.|\d]/.test(arr[0])&&/[\.|\d]/.test(arr[arr.length-1]))){
-        console.log('pass regex: '+num)
         res = equation(num)
-        console.log(res)
         return res;
       }
       else if(arr.length < 2){
--- routes/api.js
+++ routes/api.js
@@ -12,27 +12,28 @@ const valUnit = (u) => {
 module.exports = function (app) {  
   let convertHandler = new ConvertHandler();
   const startConvert = (num,unit,res) => {
-    // console.log(num,unit)
+    console.log(num,unit)
     // res.json({unit:unit,number:num})
     let n = myEval(convertHandler.getNum(num)) // initNum
     let u = convertHandler.getUnit(unit) //initUnit
     let return_n = convertHandler.convert(n,u)
     let return_u = convertHandler.getReturnUnit(u) //return unit
     let string = convertHandler.getString(n,u,return_n,return_u)
-    // console.log(typeof n)
-    // console.log(n)
-    // console.log(u)
-    // console.log(return_u)
-    // console.log(return_n)
-    // console.log(string)
     res.send(string)
   }
 
+
+
+
+
+
+
+
   app.get('/api/convert',(req,res)=>{
     const { input } = req.query;
-    let num = (input.match(/[^a-z]/gi)||[1]).join`` // get numbers in eval form
+    let num = (input.match(/[^a-zA-Z]/g)||[1]).join`` // get numbers in eval form
     let unit = (input.match(/[a-z]/gi)||[]).join`` // get units
-
+    console.log('real num: '+num)
     try {
       if(!myEval(num)&&!valUnit(unit)){
         res.send('invalid number and unit')
